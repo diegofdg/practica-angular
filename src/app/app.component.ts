84e25e3275544a7bfc1c369f317e8b28378cc158
diegofdg <diegofdg@gmail.com>
2023-07-12 21:37:39 -0300

    tarea terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  Nombre: string;
+  Costo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'material-app';
+
+  items: Item[] = [
+    { Nombre: 'Notebook', Costo: '$1000' },
+    { Nombre: 'PC Escritorio', Costo: '$1500' },
+    { Nombre: 'Impresora', Costo: '$500' },
+    { Nombre: 'Monitor', Costo: '$300' }
+  ];
+
+  mostrarLista = true;
+
+  toggleLista() {
+    this.mostrarLista = !this.mostrarLista;
+  }
+
+  columnas: string[] = [
+    'Nombre',
+    'Costo'
+  ];
 }
